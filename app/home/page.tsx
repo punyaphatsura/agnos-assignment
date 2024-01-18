73f2506f1a5c18eb7481054ad0fd1e687ee663dd
punyaphatsura <punyaphat.sura@gmail.com>
2024-01-18 09:01:51 +0700

    initial commit

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogoH from '@public/next.svg';
+
+const page = () => {
+  return (
+    <div className="p-8">
+      <LogoH />
+    </div>
+  );
+};
+
+export default page;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      type: 'asset',
+      resourceQuery: /url/, // *.svg?url
+    });
 
-module.exports = nextConfig
+    config.module.rules.push({
+      test: /\.svg$/,
+      resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
